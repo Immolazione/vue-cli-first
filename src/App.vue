5c6ca2b2f7163c609513dd790f66e10166c3696d
Immolazione <federico.deered@gmail.com>
2022-02-07 16:33:07 +0100

    link jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
+<div>
   <Title />
+  <Jumbotron />
+</div>
 </template>
 
 <script>
-import Title from "./components/Title.vue"
+import Title from "./components/Title.vue";
+import Jumbotron from './components/Jumbotron.vue';
 export default {
-  Title
+  name: 'App', 
+  components:{
+    Title,
+    Jumbotron,
+  }
 }
 </script>
 
