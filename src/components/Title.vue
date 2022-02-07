036d74bee209e334eacd8857a0709f125bd5cd5e
Immolazione <federico.deered@gmail.com>
2022-02-07 16:09:46 +0100

    add title

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,12 +1,16 @@
 <template>
-
+    <h1>La mia prima App con Vue!</h1>
 </template>
 
 <script>
 export default {
+    name: Title
 }
 </script>
 
 
 <style>
+    h1{
+        color: red;
+    }
 </style>
